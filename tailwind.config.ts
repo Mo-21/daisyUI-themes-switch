d52e063db627badd296fa4767b56c2a01c414ffd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-07-04 00:17:55 +0300

    Remove unnecessary effects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["winter", "black", "cupcake", "light", "forest", "cyberpunk"],
